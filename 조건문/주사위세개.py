f82861eef255c9cc1f17a868000a3cdcfc6df4b3
Kim-Gimin <kimjjang1997@gmail.com>
2023-06-01 11:51:16 +0900

    조건문

--- 조건문/주사위세개.py
+++ 조건문/주사위세개.py
@@ -1,1 +1,9 @@
 a,b,c = map(int, input().split())
+if a == b and b == c:
+    print(10000 + a * 1000)
+elif (a == b and b != c) or (a == c and a != b):
+    print(1000 + a * 100)
+elif b == c and a != b:
+    print(1000 + b * 100)
+elif a != b and b != c and a != b:
+    print(max(a, b, c)*100)
