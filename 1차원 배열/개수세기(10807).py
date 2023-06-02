74ee04bbabf1e74d1dc7f059f2dd988a71ba848f
Kim-Gimin <kimjjang1997@gmail.com>
2023-06-02 17:27:04 +0900

    2023.06.02

--- 1차원 배열/개수세기(10807).py
+++
@@ -1,9 +0,0 @@
-N = int(input())
-numbers = list(map(int, input().split()))
-v = int(input())
-count = 0
-for i in numbers :
-    if i == v:
-        count += 1
-
-print(count)
---
+++ 1차원 배열/최댓값(2562).py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+g = int(input())
+h = int(input())
+i = int(input())
+
+nums = [a, b, c, d, e, f, g, h, i]
+
+print(max(nums))
+print(nums.index(max(nums))+1)
