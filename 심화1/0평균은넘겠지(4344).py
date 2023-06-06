10b13e76f7b469d622ecd5c8c62593ea61f25c9a
Kim-Gimin <kimjjang1997@gmail.com>
2023-06-06 23:18:01 +0900

    23.06.06 다시공부
    파이썬 소수점자리 출력
    len으로 리스트 중 일부만 뽑기
    한번에 리스트 잡기 등 다시공부

---
+++ 심화1/0평균은넘겠지(4344).py
@@ -0,0 +1,13 @@
+C = int(input())
+
+for _ in range (C):
+    data  = list(map(int, input().split()))
+    N = data[0]
+    score = data[1:]
+    
+    average = sum(score) / N
+    
+    up = len([A for A in score if A > average ])
+    
+    ratio = up / N * 100
+    print("{:.3f}".format(ratio))
