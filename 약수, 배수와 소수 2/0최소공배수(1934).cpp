6741857b191043cf2c6a117bd2ab11ea83501201
Kim-Gimin <132413611+Kim-Gimin@users.noreply.github.com>
2023-08-03 17:14:14 +0900

    23.08.03

--- 약수, 배수와 소수 2/0최소공배수(1934).cpp
+++ 약수, 배수와 소수 2/0최소공배수(1934).cpp
@@ -1,1 +1,30 @@
+#include <iostream>
 
+int gcd(int a, int b);
+int lcm(int a, int b);
+
+int main() {
+	int T;
+	std::cin >> T;
+
+	while (T--) {
+		int A, B;
+		std::cin >> A >> B;
+		std::cout << lcm(A, B) << "\n";
+	}
+
+	return 0;
+}
+
+int gcd(int a, int b) {
+	while (b != 0) {
+		int temp = b;
+		b = a % b;
+		a = temp;
+	}
+	return a;
+}
+
+int lcm(int a, int b) {
+	return a * b / gcd(a, b);
+}
